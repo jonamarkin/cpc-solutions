981b625c576bc5efb2a973b46a2d2bd0a9d79ee7
Jonathan Ato Markin <jona10711@gmail.com>
2023-01-24 18:26:07 +0100

    sliding window maximum brute force solution added

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ fn main() {
     println!("Hello, world!");
     let array = [-1, 5, 8, -9, 4, 1];
 
-    let aa = vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+    let aa = vec![1, 3, -1, -3, 5, 3, 6, 7];
 
-    //println!("{}", trap(aa));
+    //println!("{}", sliding_window_maximum_brute_force(aa, 3));
+    //println!("{:?}", sliding_window_maximum_brute_force(aa, 3));
 }
-
-pub fn
-
--- src/sliding_window_maximum.rs
+++ src/sliding_window_maximum.rs
@@ -1,1 +1,31 @@
+//Iterate over all the sliding windows and return the maximum for each
+//There are  N-k+1 sliding windows
+//There are k elements in the window
+//Time complexity is 0(Nk)
+pub fn sliding_window_maximum_brute_force(nums: Vec<i32>, k: usize) -> Vec<i32> {
+    let n = nums.len();
 
+    //Handle if nums is empty or k is zero
+    if n * k == 0 {
+        return vec![0];
+    }
+
+    //Variable to hold the results and initialize with the number of windows
+    let mut results: Vec<i32> = vec![0; n - k + 1];
+
+    //Iterate over all the windows
+    for i in 0..(n - k + 1) {
+        //For every window
+        //Initialize a variable to hold the max in this current window
+        let mut max = i32::MIN;
+
+        //Loop again to Compare with other elements in the window to get the max in te window
+        for j in i..i + k {
+            max = std::cmp::max(max, nums[j]);
+        }
+
+        results[i] = max;
+    }
+
+    return results;
+}
