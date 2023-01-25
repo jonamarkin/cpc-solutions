4beb37886758b29d6af84b3b528453e3ca0ecf2e
Jonathan Ato Markin <jona10711@gmail.com>
2023-01-25 12:23:34 +0100

    Sliding window max deque solution added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 fn main() {
     println!("Hello, world!");
-    let array = [-1, 5, 8, -9, 4, 1];
+    let array = [1, 3, -1, -3, 5, 3, 6, 7];
 
     let aa = vec![1, 3, -1, -3, 5, 3, 6, 7];
 
     //println!("{}", sliding_window_maximum_brute_force(aa, 3));
-    //println!("{:?}", sliding_window_maximum_brute_force(aa, 3));
+    println!("{:?}", max_sliding_window(&array, 3));
+}
+
+pub fn max_sliding_window(nums: &[i32], k: usize) -> Vec<i32> {
+    // Edge cases check: if length of nums is 0 or k is 0 or 1 return empty vec
+    let n = nums.len();
+    if n * k == 0 {
+        return vec![];
+    }
+    if k == 1 {
+        return nums.to_vec();
+    }
+
+    // Initialize deque with first k elements indexes
+    let mut deq: Vec<usize> = (0..k).collect();
+    // Find the index of the max element in the first k elements
+    let mut max_idx = (0..k).max_by(|&i, &j| nums[i].cmp(&nums[j])).unwrap();
+    // Initialize the output with the max element in the first k elements
+    let mut output = vec![nums[max_idx]];
+
+    // Iterate through the nums vector starting from the k-th element
+    for i in k..n {
+        //Remove the first element from the deque if it's not in the current sliding window
+        if deq[0] == i - k {
+            deq.remove(0);
+        }
+        //Remove elements from deque that are smaller than current element
+        while !deq.is_empty() && nums[i] > nums[*deq.last().unwrap()] {
+            deq.pop();
+        }
+        // Add current element to the deque
+        deq.push(i);
+        // Add the max element of the current sliding window to the output
+        output.push(nums[deq[0]]);
+    }
+    output
 }
--- src/sliding_window_maximum.rs
+++ src/sliding_window_maximum.rs
@@ -29,3 +29,49 @@ pub fn sliding_window_maximum_brute_force(nums: Vec<i32>, k: usize) -> Vec<i32> 
 
     return results;
 }
+
+//Using Deque to make the solution better
+//This approach uses functional programming
+//I pass a reference to the nums array in this case
+// The time complexity of this approach is O(n), where n is the number of elements in the input nums vector.
+// This is because the max_sliding_window_deque function iterates over each element of nums exactly once, performing a constant amount of work for each element.
+// The space complexity of this approach is O(k), where k is the size of the sliding window.
+// This is because the deq vector is used to store the indexes of the elements in the sliding window and its size is limited to k elements at any given time.
+// The collect method's time complexity is O(n) where n is the number of elements in the iterator and space complexity is O(n) as it creates a new vector with n elements.
+// The max_by method's time complexity is O(n) and space complexity is O(1). The remove(index) and push method's time complexity is O(n) and space complexity is O(1) as they insert/remove elements from vector.
+// So overall the time and space complexity of this approach is O(n) and O(k) respectively which makes it quite efficient for large inputs.
+
+pub fn max_sliding_window_deque(nums: &[i32], k: usize) -> Vec<i32> {
+    // Edge cases check: if length of nums is 0 or k is 0 or 1 return empty vec
+    let n = nums.len();
+    if n * k == 0 {
+        return vec![];
+    }
+    if k == 1 {
+        return nums.to_vec();
+    }
+
+    // Initialize deque with first k elements indexes
+    let mut deq: Vec<usize> = (0..k).collect();
+    // Find the index of the max element in the first k elements
+    let mut max_idx = (0..k).max_by(|&i, &j| nums[i].cmp(&nums[j])).unwrap();
+    // Initialize the output with the max element in the first k elements
+    let mut output = vec![nums[max_idx]];
+
+    // Iterate through the nums vector starting from the k-th element
+    for i in k..n {
+        //Remove the first element from the deque if it's not in the current sliding window
+        if deq[0] == i - k {
+            deq.remove(0);
+        }
+        //Remove elements from deque that are smaller than current element
+        while !deq.is_empty() && nums[i] > nums[*deq.last().unwrap()] {
+            deq.pop();
+        }
+        // Add current element to the deque
+        deq.push(i);
+        // Add the max element of the current sliding window to the output
+        output.push(nums[deq[0]]);
+    }
+    output
+}
