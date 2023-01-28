fe4515349fb62e68e5e20f74fda11a0e3b394bb5
Jonathan Ato Markin <jona10711@gmail.com>
2023-01-28 15:19:35 +0100

    Number of ways corrected

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,49 @@
-fn main() {}
+use std::{io, num};
+
+fn number_of_ways(list: &[i64]) -> u64 {
+    let mut s: i64 = list.iter().fold(0i64, |a, b| a + (*b as i64));
+
+    if s % 3 != 0 {
+        return 0;
+    }
+
+    s /= 3;
+
+    let mut cnt = vec![0; list.len()];
+    let mut ss: i64 = 0;
+    for i in (0..=list.len() - 1).rev() {
+        ss += list[i] as i64;
+        if ss == s {
+            cnt[i] = 1;
+        }
+    }
+    for i in (0..=list.len() - 2).rev() {
+        cnt[i] += cnt[i + 1];
+    }
+
+    let mut result = 0;
+    ss = 0;
+    for i in 0..list.len() - 2 {
+        ss += list[i] as i64;
+        if ss == s {
+            result += cnt[i + 2];
+        }
+    }
+
+    result
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: usize = input.trim().parse().unwrap();
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+    let a: Vec<i64> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    println!("{}", number_of_ways(&a));
+}
--- src/number_of_ways.rs
+++ src/number_of_ways.rs
@@ -2,59 +2,52 @@
 //Question Link: https://codeforces.com/problemset/problem/466/C?locale=en
 //Rationale behind the code
 //Prefix Sum Approach
-// It starts by reading the input, which is the number of elements in the array and the elements themselves.
-// Then it checks if the sum of the elements is divisible by 3, if not it prints 0 and exits.
+use std::{io, num};
 
-// It then initializes two variables, suf and dp, and starts a loop that iterates through the array in reverse.
-// In each iteration, it updates the suf variable with the current element of the array, and updates the dp array with the previous element if the current index is not the last element.
-// If the suf is equal to one-third of the total sum, it increments the current element of the dp array.
+fn number_of_ways(list: &[i64]) -> u64 {
+    let mut s: i64 = list.iter().fold(0i64, |a, b| a + (*b as i64));
 
-// Then it initializes an ans variable to zero and starts another loop that iterates through the array in the forward direction.
-// In each iteration, it updates the pre variable with the current element of the array, and if the pre is equal to one-third of the total sum, it increments the ans variable with the corresponding element of the dp array.
-// Finally, it prints the ans variable as the output.
+    if s % 3 != 0 {
+        return 0;
+    }
 
-use std::io::{self, Read};
+    s /= 3;
+
+    let mut cnt = vec![0; list.len()];
+    let mut ss: i64 = 0;
+    for i in (0..=list.len() - 1).rev() {
+        ss += list[i] as i64;
+        if ss == s {
+            cnt[i] = 1;
+        }
+    }
+    for i in (0..=list.len() - 2).rev() {
+        cnt[i] += cnt[i + 1];
+    }
+
+    let mut result = 0;
+    ss = 0;
+    for i in 0..list.len() - 2 {
+        ss += list[i] as i64;
+        if ss == s {
+            result += cnt[i + 2];
+        }
+    }
+
+    result
+}
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
-    let mut iter = input.split_whitespace();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: usize = input.trim().parse().unwrap();
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+    let a: Vec<i64> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
 
-    let n: i32 = iter.next().unwrap().parse().unwrap();
-    let mut a = Vec::new();
-    let mut sum = 0;
-    for _ in 0..n {
-        let x: i32 = iter.next().unwrap().parse().unwrap();
-        a.push(x);
-        sum += x;
-    }
-
-    if sum % 3 != 0 {
-        println!("0");
-        return;
-    }
-
-    let sum = sum / 3;
-    let mut dp = vec![0; n as usize];
-    let mut suf = 0;
-    for i in (0..n).rev() {
-        suf += a[i as usize];
-        if i < n - 1 {
-            dp[i as usize] = dp[i as usize + 1];
-        }
-        if suf == sum {
-            dp[i as usize] += 1;
-        }
-    }
-
-    let mut ans = 0;
-    let mut pre = 0;
-    for i in 0..n - 2 {
-        pre += a[i as usize];
-        if pre == sum {
-            ans += dp[i as usize + 2];
-        }
-    }
-
-    println!("{}", ans);
+    println!("{}", number_of_ways(&a));
 }
